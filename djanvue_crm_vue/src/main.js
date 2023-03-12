97fdd34d0f02340f4c8259d86612d20bfbb8122f
eric <eric.blandet@gmail.com>
2023-03-12 22:35:52 +0100

    requirements.txt

--- djanvue_crm_vue/src/main.js
+++ djanvue_crm_vue/src/main.js
@@ -1,9 +1,9 @@
+import axios from "axios";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.baseURL = process.env.VUE_APP_BASE_SERVER_URL;
 
 createApp(App).use(store).use(router, axios).mount("#app");
