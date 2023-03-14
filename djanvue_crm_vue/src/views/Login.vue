15b04db245b965dd850969c572cd0004b31f662e
eric <eric.blandet@gmail.com>
2023-03-14 17:14:02 +0100

    plan not necessary

--- djanvue_crm_vue/src/views/Login.vue
+++ djanvue_crm_vue/src/views/Login.vue
@@ -8,15 +8,24 @@
           <div class="field">
             <label for="email">Email</label>
             <div class="control">
-              <input type="email" name="email" class="input" v-model="username" />
+              <input
+                type="email"
+                name="email"
+                class="input"
+                v-model="username"
+              />
             </div>
           </div>
 
           <div class="field">
             <label for="password">Password</label>
             <div class="control">
-              <input type="password" name="password1" class="input" v-model="password" />
-
+              <input
+                type="password"
+                name="password1"
+                class="input"
+                v-model="password"
+              />
             </div>
           </div>
 
@@ -36,80 +45,83 @@
 </template>
 
 <script>
-import axios from 'axios';
+  import axios from "axios";
 
+  export default {
+    name: "Login",
+    data() {
+      return {
+        username: "",
+        password: "",
+        errors: [],
+      };
+    },
+    methods: {
+      async submitForm() {
+        this.$store.commit("setIsLoading", true);
 
-export default {
-  name: "Login",
-  data() {
-    return {
-      username: '',
-      password: '',
-      errors: []
-    }
-  },
-  methods: {
-    async submitForm() {
-      this.$store.commit('setIsLoading', true)
+        axios.defaults.headers.common["Authorization"] = "";
+        localStorage.removeItem("token");
+        this.errors = [];
 
-      axios.defaults.headers.common['Authorization'] = ''
-      localStorage.removeItem('token')
-      this.errors = []
-
-      let formData = {}
-      if (!this.errors.length) {
-        formData = {
-          username: this.username,
-          password: this.password,
+        let formData = {};
+        if (!this.errors.length) {
+          formData = {
+            username: this.username,
+            password: this.password,
+          };
         }
-      }
 
-      await axios.post('/api/v1/token/login/', formData).then(
-        (response) => {
-          const token = response.data.auth_token
-          this.$store.commit('setToken', token)
-          axios.defaults.headers.common['Authorization'] = 'Token ' + token
-          localStorage.setItem('token', token)
-
-        }).catch(error => {
-          if (error.response) {
-            for (const property in error.response.data) {
-              this.errors.push(`${property}: ${error.response.data[property]}`)
-            }
-          } else if (error.message) {
-            this.errors.push('Something went wrong, please try again')
-          }
-        })
-
-      await axios
-        .get('/api/v1/users/me/')
-        .then(response => {
-          this.$store.commit('setUser', { id: response.data.id, username: response.data.username })
-
-          localStorage.setItem('username', response.data.username)
-          localStorage.setItem('user_id', response.data.id)
-
-        })
-        .catch(error =>
-          console.log(error))
-
-      await axios
-        .get('/api/v1/teams/get_my_team/')
-        .then(response => {
-          this.$store.commit('setTeam', {
-            id: response.data.id, name: response.data.name,
-            plan: response.data.plan.name,
-            max_leads: response.data.plan.max_leads,
-            max_clients: response.data.plan.max_clients,
-            plan_end_date: response.data.plan_end_date,
+        await axios
+          .post("/api/v1/token/login/", formData)
+          .then((response) => {
+            const token = response.data.auth_token;
+            this.$store.commit("setToken", token);
+            axios.defaults.headers.common["Authorization"] = "Token " + token;
+            localStorage.setItem("token", token);
           })
-          this.$router.push('/dashboard/my-account')
-        })
-        .catch(error =>
-          console.log(error))
+          .catch((error) => {
+            if (error.response) {
+              for (const property in error.response.data) {
+                this.errors.push(
+                  `${property}: ${error.response.data[property]}`
+                );
+              }
+            } else if (error.message) {
+              this.errors.push("Something went wrong, please try again");
+            }
+          });
 
-      this.$store.commit('setIsLoading', false)
-    }
-  }
-};
+        await axios
+          .get("/api/v1/users/me/")
+          .then((response) => {
+            this.$store.commit("setUser", {
+              id: response.data.id,
+              username: response.data.username,
+            });
+
+            localStorage.setItem("username", response.data.username);
+            localStorage.setItem("user_id", response.data.id);
+          })
+          .catch((error) => console.log(error));
+
+        await axios
+          .get("/api/v1/teams/get_my_team/")
+          .then((response) => {
+            this.$store.commit("setTeam", {
+              id: response.data.id,
+              name: response.data.name,
+              plan: response.data?.plan?.name,
+              max_leads: response.data?.plan.max_leads,
+              max_clients: response.data?.plan.max_clients,
+              plan_end_date: response.data?.plan_end_date,
+            });
+            this.$router.push("/dashboard/my-account");
+          })
+          .catch((error) => console.log(error));
+
+        this.$store.commit("setIsLoading", false);
+      },
+    },
+  };
 </script>
