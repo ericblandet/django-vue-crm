71fa6920007124d1c3406921e7770c498fdff25c
eric <eric.blandet@gmail.com>
2023-03-16 12:57:45 +0100

    target blank for external link

--- djanvue_crm_vue/src/views/dashboard/MyAccount.vue
+++ djanvue_crm_vue/src/views/dashboard/MyAccount.vue
@@ -15,7 +15,9 @@
         </div>
         <div class="">
           <span>Application administrator ?</span>
-          <a :href="`${$store.state.apiUrl}/admin`"> Go to Admin dashboard</a>
+          <a :href="`${$store.state.apiUrl}admin`" target="_blank" no>
+            Go to Admin dashboard</a
+          >
         </div>
       </div>
     </div>
